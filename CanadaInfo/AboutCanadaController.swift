7e4e584b1ef971f51ad26326426cf8fc3b098477
saleelkarkhanis <karkhanis.saleel@gmail.com>
2018-02-21 19:44:14 +0530

    Renamed initial View Controller and Added Navigation controller to it.

---
+++ CanadaInfo/AboutCanadaController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CanadaInfo
+//
+//  Created by synerzip on 21/02/18.
+//  Copyright © 2018 saleelkarkhanis. All rights reserved.
+//
+
+import UIKit
+
+class AboutCanadaController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
